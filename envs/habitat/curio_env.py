b572382e464fa97ae48d1a04432bb5466c5489bb
yuanxuanS <panpanono@163.com>
2025-01-07 12:40:48 +0800

    Version1

---
+++ envs/habitat/curio_env.py
@@ -0,0 +1,146 @@
+import numpy as np
+import gym
+import habitat
+import quaternion
+
+
+class Seman_Curio_Env(habitat.RLEnv):
+    """The Semantic Curiosity environment class. The class is responsible
+    for loading the dataset, generating episodes, and computing evaluation
+    metrics.
+    """
+    def __init__(self, args, rank, config_env, dataset):
+        self.args = args
+        self.rank = rank
+
+        super().__init__(config_env, dataset)
+
+        # Loading dataset info file
+        self.split = config_env.DATASET.SPLIT
+        
+        # Specifying action and observation space
+        self.action_space = gym.spaces.Discrete(3)
+
+        self.observation_space = gym.spaces.Box(0, 255,
+                                                (3, args.frame_height,
+                                                 args.frame_width),
+                                                dtype='uint8')
+
+        # Scene info
+        self.last_scene_path = None
+        self.scene_path = None
+        self.scene_name = None
+
+        # episode tracking into
+        self.timestep = None
+        self.info = {}
+        
+        # episode id 
+        self.episode_no = 0
+
+        
+    def reset(self):
+        """Resets the environment to a new episode."""
+
+        # Initializations
+        self.timestep = 0
+        self.episode_no += 1
+
+        obs = super().reset()
+        rgb = obs['rgb'].astype(np.uint8)
+        depth = obs['depth']
+        state = np.concatenate((rgb, depth), axis=2).transpose(2, 0, 1)
+
+
+        # Set info
+        self.info['time'] = self.timestep
+        self.info['sensor_pose'] = [0., 0., 0.]
+
+
+        return state, self.info
+    
+    def step(self, action):
+        """Function to take an action in the environment.
+
+        Args:
+            action (dict):
+                dict with following keys:
+                    'action' (int): 1: forward, 2: left, 3: right
+
+        Returns:
+            obs (ndarray): RGBD observations (4 x H x W)
+            reward (float): amount of reward returned after previous action
+            done (bool): whether the episode has ended
+            info (dict): contains timestep, pose, goal category and
+                         evaluation metric info
+        """
+
+        action = action["action"]
+
+        # step
+        obs, _, done, _ = super().step(action)
+
+        # TODO: Get pose change?
+        dx, dy, do = self.get_pose_change()
+        self.info['sensor_pose'] = [dx, dy, do]
+
+
+        # self.update_semantic_map(obs, poses, local_map, local_pose)
+
+        rgb = obs['rgb'].astype(np.uint8)
+        depth = obs['depth']
+        state = np.concatenate((rgb, depth), axis=2).transpose(2, 0, 1)
+
+        self.timestep += 1
+        self.info['time'] = self.timestep
+
+        return state, None, done, self.info
+    
+    def get_reward_range(self):
+        """This function is not used, Habitat-RLEnv requires this function"""
+        return (0., 1.0)
+    
+    def get_reward(self):
+        # not used
+        return None
+    
+
+
+    def get_done(self):
+        if self.info['time'] >= self.args.max_episode_length:       # 
+            done = True
+        else:
+            done = False
+        return done
+    
+    def get_pose_change(self):
+        """Returns dx, dy, do pose change of the agent relative to the last
+        timestep."""
+        curr_sim_pose = self.get_sim_location()
+        dx, dy, do = pu.get_rel_pose_change(
+            curr_sim_pose, self.last_sim_location)
+        self.last_sim_location = curr_sim_pose
+        return dx, dy, do
+    
+    def get_sim_location(self):
+        """Returns x, y, o pose of the agent in the Habitat simulator."""
+
+        agent_state = super().habitat_env.sim.get_agent_state(0)
+        x = -agent_state.position[2]
+        y = -agent_state.position[0]
+        axis = quaternion.as_euler_angles(agent_state.rotation)[0]
+        if (axis % (2 * np.pi)) < 0.1 or (axis %
+                                          (2 * np.pi)) > 2 * np.pi - 0.1:
+            o = quaternion.as_euler_angles(agent_state.rotation)[1]
+        else:
+            o = 2 * np.pi - quaternion.as_euler_angles(agent_state.rotation)[1]
+        if o > np.pi:
+            o -= 2 * np.pi
+        return x, y, o
+    
+    def get_action_space(self):
+        return self.action_space
+    
+    def get_obs_space(self):
+        return self.observation_space
+
---
+++ main.py
@@ -0,0 +1,316 @@
+
+from arguments import get_args
+import torch
+import numpy as np
+import os
+import logging
+from collections import deque, defaultdict
+import gym
+import time
+
+from envs import make_vec_envs
+from maps import Maps_Env
+from utils.storage import GlobalRolloutStorage
+from model import RL_Policy
+import algo
+
+
+def main():
+    args = get_args()
+    
+    # seed 
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
+
+    # Setup Logging
+    log_dir = "{}/models/{}/".format(args.dump_location, args.exp_name)
+    dump_dir = "{}/dump/{}/".format(args.dump_location, args.exp_name)
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    if not os.path.exists(dump_dir):
+        os.makedirs(dump_dir)
+    
+    log_name = 'eval.log' if args.eval else 'train.log' 
+    logging.basicConfig(
+        filename=log_dir + log_name,
+        level=logging.INFO)
+    print("Dumping at {}".format(log_dir))
+    print(args)
+    logging.info(args)
+
+    # Logging and loss variables
+    num_scenes = args.num_processes
+    device = args.device = torch.device("cuda:0" if args.cuda else "cpu")   # 训练的gpu
+
+    #  l_masks, not used. episode length不同时使用
+    l_masks = torch.ones(num_scenes).float().to(device)
+
+    best_g_reward = -np.inf
+
+    if args.eval:
+        pass # TODO: 增加一些online指标
+    else:
+        pass # TODO: 
+
+    l_episode_rewards = deque(maxlen=1000)
+    l_process_rewards = np.zeros((num_scenes))
+
+    l_value_losses = deque(maxlen=1000)
+    l_action_losses = deque(maxlen=1000)
+    l_dist_entropies = deque(maxlen=1000)
+
+    # Starting environments
+    torch.set_num_threads(1)
+    envs = make_vec_envs(args)      
+    obs, infos = envs.reset()   # obs: rgb +depth + categories 16 TODO: ?
+
+    torch.set_grad_enabled(False)
+
+    # Initializing Maps
+    # Full map consists of multiple channels containing the following:
+    # 1. Obstacle Map
+    # 2. Exploread Area
+    # 3. Current Agent Location
+    # 4. Past Agent Locations
+    # 5,6,7,.. : Semantic Categories
+    maps = Maps_Env(args)
+    local_map, local_pose = maps.update_semantic_map(obs, infos)
+
+    # Local policy observation space
+    es = 1      # extra size: orientation
+    l_observation_space = envs.get_obs_space()  # TODO: VectorEnv's func
+    l_action_space = envs.get_action_space()
+
+    # local policy recurrent layer size
+    l_hidden_size = args.local_hidden_size
+
+    # Local policy: TODO
+    l_policy = RL_Policy(l_observation_space.shape, l_action_space,
+                         model_type=1,
+                         base_kwargs={'recurrent': args.use_recurrent_local,
+                                      'hidden_size': l_hidden_size,
+                                      'num_sem_categories': args.num_sem_categories
+                                      }).to(device)
+    
+    l_agent = algo.PPO(l_policy, args.clip_param, args.ppo_epoch,
+                       args.num_mini_batch, args.value_loss_coef,
+                       args.entropy_coef, lr=args.lr, eps=args.eps,
+                       max_grad_norm=args.max_grad_norm)
+
+    
+
+    # Storage: 
+    l_rollouts = GlobalRolloutStorage(args.num_local_steps,
+                                      num_scenes, l_observation_space.shape,
+                                      l_action_space, l_policy.rec_state_size,
+                                      es).to(device)
+    
+    # load weights
+    if args.load != "0":
+        print("Loading model {}".format(args.load))
+        state_dict = torch.load(args.load,
+                                map_location=lambda storage, loc: storage)
+        l_policy.load_state_dict(state_dict)
+
+    if args.eval:
+        l_policy.eval()
+
+
+    # Get local policy input
+    local_input = obs[:, :3, ...]
+    local_orientation = torch.zeros(num_scenes, 1).long()
+
+    locs = local_pose.cpu().numpy()
+    for e in range(num_scenes):
+        local_orientation[e] = int((locs[e, 2] + 180.0) / 5.)
+
+    extras = torch.zeros(num_scenes, es)
+    extras[:, 0] = local_orientation[:, 0]
+
+    l_rollouts.obs[0].copy_(local_input)   # 
+    l_rollouts.extras[0].copy_(extras)
+
+    # Run Local policy
+    l_value, l_action, l_action_log_prob, l_rec_states = \
+        l_policy.act(
+            l_rollouts.obs[0],
+            l_rollouts.rec_states[0],
+            l_rollouts.masks[0],
+            extras=l_rollouts.extras[0],
+            deterministic=False
+        )
+    l_action = l_action.cpu()
+    
+    
+    # for visualize
+    vis_inputs = [{} for e in range(num_scenes)]
+    for e, p_input in enumerate(vis_inputs):
+        p_input['map_pred'] = local_map[e, 0, :, :].cpu().numpy()
+        p_input['exp_pred'] = local_map[e, 1, :, :].cpu().numpy()
+        p_input['pose_pred'] = maps.get_all_pose()[e]
+        if args.visualize or args.print_images:
+            local_map[e, -1, :, :] = 1e-5
+            p_input['sem_map_pred'] = local_map[e, 4:, :, :
+                                                ].argmax(0).cpu().numpy()
+
+    # transition:
+    # pred instance, get semantic masks and step env: 
+    obs, _, done, infos = envs.step_and_preprocess(l_action, vis_inputs)
+    # update map
+    local_map, local_pose = maps.update_semantic_map(obs, infos)
+
+    start = time.time()
+    l_reward = torch.zeros(num_scenes).to(device)
+
+    torch.set_grad_enabled(False)
+
+    print("Starting training")
+    logging.info("Starting training")
+    print(f"training frames is {args.num_training_frames}")
+    logging.info(f"training frames is {args.num_training_frames}")
+    for step in range(args.num_training_frames // args.num_processes + 1):
+        l_step = step % args.num_local_steps
+        
+        # get reward: map change after state transition
+        if done[0]:     # maps are new obs, sum of map will be small, and get negative reward
+            l_reward = args.reward_coeff*last_reward
+        else:
+            l_reward = args.reward_coeff* maps.sum_of_semantic_map()
+
+        l_process_rewards += l_reward.cpu().numpy()
+        # per step reward? TODO
+        # add explore metric: TODO
+
+        # ------------------------------------------------------------------ 
+        # update local input, next state
+        locs = local_pose.cpu().numpy()
+        for e in range(num_scenes):
+            local_orientation[e] = int((locs[e, 2] + 180.0) / 5.)   # 
+
+        local_input = obs[:, :3, ...]       # rgb
+        extras[:, 0] = local_orientation[:, 0]
+
+        # Add samples to local policy storage
+        reward = l_reward - last_reward
+        l_rollouts.insert(
+                local_input, l_rec_states,      # state_t+1
+                l_action, l_action_log_prob, l_value,   # action, reward_t
+                reward, l_masks, extras
+            )
+        last_reward = l_reward
+
+        print(f"step: {step} , local step: {l_step}, reward={reward}")
+        logging.info(f"step: {step} , local step: {l_step}, reward={reward}")
+        
+        # Sample next action
+        l_value, l_action, l_action_log_prob, l_rec_states = \
+            l_policy.act(
+                l_rollouts.obs[l_step + 1],
+                l_rollouts.rec_states[l_step + 1],
+                l_rollouts.masks[l_step + 1],
+                extras=l_rollouts.extras[l_step + 1],
+                deterministic=False
+            )
+        l_action = l_action.cpu()
+
+        vis_inputs = [{} for e in range(num_scenes)]
+        for e, p_input in enumerate(vis_inputs):
+            p_input['map_pred'] = local_map[e, 0, :, :].cpu().numpy()
+            p_input['exp_pred'] = local_map[e, 1, :, :].cpu().numpy()
+            p_input['pose_pred'] = maps.get_all_pose[e]
+            if args.visualize or args.print_images:
+                local_map[e, -1, :, :] = 1e-5
+                p_input['sem_map_pred'] = local_map[e, 4:, :, :
+                                                    ].argmax(0).cpu().numpy()
+
+
+        # transition: next state
+        # pred instance, get semantic masks and step env
+        obs, _, done, infos = envs.step_and_preprocess(l_action, vis_inputs)    # if done ,obs are ones after reset
+        # if episode over, reset maps
+        for e, x in enumerate(done):    # if done, maps from new obs
+            if x:
+                maps._init_map_and_pose_for_env(e)
+                print(f"Env {e}'s episode over in {step} step, {l_step} local step, reset maps")
+        # update map
+        local_map, local_pose = maps.update_semantic_map(obs, infos)
+
+        # ------------------------------------------------------------------
+        # Training
+        torch.set_grad_enabled(True)
+        if l_step == args.num_local_steps - 1:
+            if not args.eval:
+                l_next_value = l_policy.get_value(
+                    l_rollouts.obs[-1],
+                    l_rollouts.rec_states[-1],
+                    l_rollouts.masks[-1],
+                    extras=l_rollouts.extras[-1]
+                ).detach()
+                l_rollouts.compute_returns(l_next_value, args.use_gae,
+                                           args.gamma, args.tau)
+                l_value_loss, l_action_loss, l_dist_entropy = \
+                    l_agent.update(l_rollouts)
+                l_value_losses.append(l_value_loss)
+                l_action_losses.append(l_action_loss)
+                l_dist_entropies.append(l_dist_entropy)
+            l_rollouts.after_update()       # rollout的最后一个state是下一次initial state
+            
+            # reset 
+            l_process_rewards = np.zeros((num_scenes))
+            l_reward = torch.zeros(num_scenes).to(device)
+
+        torch.set_grad_enabled(False)
+
+        # ------------------------------------------------------------------
+        # Logging: TODO
+        if step % args.log_interval == 0:
+            end = time.time()
+            time_elapsed = time.gmtime(end - start)
+            log = " ".join([
+                "Time: {0:0=2d}d".format(time_elapsed.tm_mday - 1),
+                "{},".format(time.strftime("%Hh %Mm %Ss", time_elapsed)),
+                "num timesteps {},".format(step * num_scenes),
+                "FPS {},".format(int(step * num_scenes / (end - start)))
+            ])
+
+            log += "\n\tLosses:"
+            if len(l_value_losses) > 0 and not args.eval:
+                log += " ".join([
+                    " Policy Loss value/action/dist:",
+                    "{:.3f}/{:.3f}/{:.3f},".format(
+                        np.mean(l_value_losses),
+                        np.mean(l_action_losses),
+                        np.mean(l_dist_entropies))
+                ])
+
+
+        # ------------------------------------------------------------------
+        # Save best models
+        if (step * num_scenes) % args.save_interval < \
+                num_scenes:
+            if len(l_episode_rewards) >= 1000 and \
+                    (np.mean(l_episode_rewards) >= best_g_reward) \
+                    and not args.eval:
+                torch.save(l_policy.state_dict(),
+                           os.path.join(log_dir, "model_best.pth"))
+                best_g_reward = np.mean(l_episode_rewards)
+        # Save periodic models
+        if (step * num_scenes) % args.save_periodic < \
+                num_scenes:
+            total_steps = step * num_scenes
+            if not args.eval:
+                torch.save(l_policy.state_dict(),
+                           os.path.join(dump_dir,
+                                        "periodic_{}.pth".format(total_steps)))
+        # ------------------------------------------------------------------
+    # Print and save model performance numbers during evaluation: TODO
+    if args.eval:
+        print("Dumping eval details...")
+        # TODO
+
+if __name__ == "__main__":
+    main()
