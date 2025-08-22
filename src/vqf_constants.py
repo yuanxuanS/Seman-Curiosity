6060ecc20637e70e3b37bd1fd6cc3aaeba474966
yuanxuanS <panpanono@163.com>
2025-08-22 16:47:32 +0800

    add VQF network

---
+++ src/vqf_constants.py
@@ -0,0 +1,24 @@
+
+target_classes = ['chair', 'bed', 'toilet', 'couch', 'refrigerator']
+target_cls_id_in_scene = [0, 1, 3, 4, 9]
+
+target_coco_categories = {
+    "chair": 0,
+    "couch": 1,
+    # "potted plant": 2,
+    "bed": 3,
+    "toilet": 4,
+    # "tv": 5,
+    # "dining-table": 6,
+    # "oven": 7,
+    # "sink": 8,
+    "refrigerator": 9,
+    # "book": 10,
+    # "clock": 11,
+    # "vase": 12,
+    # "cup": 13,
+    # "bottle": 14
+}
+
+category_maps = {0: "chair", 1:"couch", 3:"bed", 4:"toilet", 9:"refrigerator"}
+category_id_maps = {0: 56, 1:57, 3:59, 4:61, 9:72}
--- test_label.py
+++ test_label.py
@@ -70,165 +70,174 @@ def manual_bin_count(data, bin_size=0.1):
 
 if __name__ == "__main__":
 
-    mode = "read"   #"predict"  #
+    mode = "predict"  # "read"   #
     predict_mode = "clip"       # "maskrcnn"
-    class_name = "refrigerator"
+    class_names = ["chair", "bed", "couch", "refrigerator", "toilet"]
+    save_dir = "/data2/wpp_data/obj_azimuth/"
     preds = {}
     values = {}
     cnt_none = {}
     cnt_false = {}
+    distances = ['2m', '2.5m', '3m', '3.5m', '4m', '4.5m', '5m', '5.5m']
     
-    if mode == "predict":
-        if predict_mode == "maskrcnn":
-            args = get_args()
-            sem_pred = SemanticPredMaskRCNN(args)
-            pred_instances = {}
-        elif predict_mode == "clip":
-            device = "cuda:3" if torch.cuda.is_available() else "cpu"
-            model, preprocess = clip.load("ViT-L/14", device=device)
-
-            text_str = [class_name]
-            text = clip.tokenize([f"a photo contains a {i}" for i in text_str]).to(device)
-
-
-        # 读取文件夹
-        dir = '/data2/wpp_data/obj_azimuth/'+class_name
-
-        tmp = 0        
-        for obj_dir in os.listdir(dir):
+    for class_name in class_names:
+        if mode == "predict":
             if predict_mode == "maskrcnn":
-                pred_instances[obj_dir] = {}
-                
-                preds[obj_dir] = {}
-                cnt_none[obj_dir] = {}
-                cnt_false[obj_dir] = {}
-            values[obj_dir] = {}
-            
-            for distance in os.listdir(dir + "/"+obj_dir):
-                values[obj_dir][distance] = []
-                if predict_mode == "maskrcnn":
-                    pred_instances[obj_dir][distance] = []
-                    preds[obj_dir][distance] = []
-                    cnt_none[obj_dir][distance] = 0
-                    cnt_false[obj_dir][distance] = 0
-                for angle in range(360):
-                    # match = re.search(r'render_(\d+).png', img_str)
-                    # angle = int(match.group(1)) if match else 1000
-                    # assert angle < 361, f"match wrong, angle {angle}"
-                    
-                    img_pth = dir+"/"+obj_dir+"/" +distance+ "/"+f"/render_{angle:03d}.png"
-                   
-                    # maskrcnn预测
-                    if predict_mode == "maskrcnn":
-                        rgb = cv2.imread(img_pth)
-                        _, _, instance = sem_pred.get_prediction(rgb, return_score=False, return_instance=True)
-                        pred_instances[obj_dir][distance].append(instance)
-                        if len(instance) == 0:
-                            values[obj_dir][distance].append(1)
-                            preds[obj_dir][distance].append(None)
-                            cnt_none[obj_dir][distance] += 1
-                        elif len(instance) == 1:
-                            if int(instance.pred_classes[0].cpu().numpy()) == 72: 
-                                values[obj_dir][distance].append(1- float(instance.scores.cpu().numpy()))
-                                preds[obj_dir][distance].append(float(instance.scores.cpu().numpy()))
-                            else:
-                                values[obj_dir][distance].append(1)
-                                cnt_false[obj_dir][distance] += 1
-                        else:
-                            values[obj_dir][distance].append(1 - float(instance.scores.mean().cpu().numpy()))
-                            preds[obj_dir][distance].append(float(instance.scores.mean().cpu().numpy()))
-                    elif predict_mode == "clip":
-                        # 预处理图像用于CLIP模型（仍使用PIL预处理）
-                        pil_img = Image.open(img_pth)
-                        image = preprocess(pil_img).unsqueeze(0).to(device)
+                args = get_args()
+                sem_pred = SemanticPredMaskRCNN(args)
+                pred_instances = {}
+            elif predict_mode == "clip":
+                device = "cuda:3" if torch.cuda.is_available() else "cpu"
+                model, preprocess = clip.load("ViT-L/14", device=device)
 
-                        with torch.no_grad():
-                            image_features = model.encode_image(image)
-                            text_features = model.encode_text(text)
-                            
-                            logits_per_image, logits_per_text = model(image, text)
-                            score = logits_per_text.item()
-                            values[obj_dir][distance].append(score)
-            #         tmp +=1
-            #         if tmp > 20:
-            #             break
-            #     if tmp > 20:
-            #         break
-            # if tmp > 20:
-            #     break
-                # clip打分
-        
-        # 存
-        if predict_mode == "maskrcnn":
-            with open(class_name+".pkl", "wb") as f:
-                pickle.dump(pred_instances, f)
-        elif predict_mode == "clip":
-            with open(class_name+"_clip.pkl", "wb") as f:
-                pickle.dump(values, f)
-            
-    if mode == "read":
-        if predict_mode == "maskrcnn":
-            with open(class_name+".pkl", "rb") as f:
-                pred_instances = pickle.load(f)
-        
-            for obj, obj_dict in pred_instances.items():
-                values[obj] = {}
-                preds[obj] = {}
-                cnt_none[obj] = {}
-                cnt_false[obj] = {}
-                for distance, instances_lst in obj_dict.items():
-                    values[obj][distance] = []
-                    preds[obj][distance] = []
-                    cnt_none[obj][distance] = 0
-                    cnt_false[obj][distance] = 0
+                text_str = [class_name]
+                text = clip.tokenize([f"a photo contains a {i}" for i in text_str]).to(device)
+
+            # 读取文件夹
+            dir = save_dir+class_name
+
+            tmp = 0        
+            for obj_dir in os.listdir(dir):
+                if predict_mode == "maskrcnn":
+                    pred_instances[obj_dir] = {}
                     
-                    for angle, instance in enumerate(instances_lst):
+                    preds[obj_dir] = {}
+                    cnt_none[obj_dir] = {}
+                    cnt_false[obj_dir] = {}
+                values[obj_dir] = {}
+                
+                for distance in distances:
+                    values[obj_dir][distance] = []
+                    if predict_mode == "maskrcnn":
+                        pred_instances[obj_dir][distance] = []
+                        preds[obj_dir][distance] = []
+                        cnt_none[obj_dir][distance] = 0
+                        cnt_false[obj_dir][distance] = 0
                         
-                        if len(instance) == 0:
-                            values[obj][distance].append(1)
-                            preds[obj][distance].append(None)
-                            cnt_none[obj][distance] += 1
-                        elif len(instance) == 1:
-                            if int(instance.pred_classes[0].cpu().numpy()) == 72: 
-                                values[obj][distance].append(1- float(instance.scores.cpu().numpy()))
-                                preds[obj][distance].append(float(instance.scores.cpu().numpy()))
-                            else:  # 错检
-                                values[obj][distance].append(1)
-                                cnt_false[obj][distance] += 1
-                        else:
-                            values[obj][distance].append(1 - float(instance.scores.mean().cpu().numpy()))
-                            preds[obj][distance].append(float(instance.scores.mean().cpu().numpy()))
+                    for angle in range(360):
+                        
+                        img_pth = dir+"/"+obj_dir+"/" +distance+ "/"+f"/render_{angle:03d}.png"
                     
-            # 统计
-            all_scores = []
-            cnt_none_all = 0
-            cnt_false_all = 0
-            for obj, obj_dict in pred_instances.items():
-                draw_array(obj, values[obj], class_name)
-                for dis, preds_scores in obj_dict.items():
-                    all_scores.extend(preds[obj][dis])
-                    cnt_none_all += cnt_none[obj][dis]
-                    cnt_false_all += cnt_false[obj][dis]
+                        # maskrcnn预测
+                        if predict_mode == "maskrcnn":
+                            rgb = cv2.imread(img_pth)
+                            _, _, instance = sem_pred.get_prediction(rgb, return_score=False, return_instance=True)
+                            pred_instances[obj_dir][distance].append(instance)
+                            if len(instance) == 0:
+                                values[obj_dir][distance].append(1)
+                                preds[obj_dir][distance].append(None)
+                                cnt_none[obj_dir][distance] += 1
+                            elif len(instance) == 1:
+                                if int(instance.pred_classes[0].cpu().numpy()) == 72: 
+                                    values[obj_dir][distance].append(1- float(instance.scores.cpu().numpy()))
+                                    preds[obj_dir][distance].append(float(instance.scores.cpu().numpy()))
+                                else:
+                                    values[obj_dir][distance].append(1)
+                                    cnt_false[obj_dir][distance] += 1
+                            else:
+                                values[obj_dir][distance].append(1 - float(instance.scores.mean().cpu().numpy()))
+                                preds[obj_dir][distance].append(float(instance.scores.mean().cpu().numpy()))
+                        elif predict_mode == "clip":
+                            # 预处理图像用于CLIP模型（仍使用PIL预处理）
+                            pil_img = Image.open(img_pth)
+                            image = preprocess(pil_img).unsqueeze(0).to(device)
+
+                            with torch.no_grad():
+                                image_features = model.encode_image(image)
+                                text_features = model.encode_text(text)
+                                
+                                logits_per_image, logits_per_text = model(image, text)
+                                score = logits_per_text.item()
+                                values[obj_dir][distance].append(score)
+                #         tmp +=1
+                #         if tmp > 20:
+                #             break
+                #     if tmp > 20:
+                #         break
+                # if tmp > 20:
+                #     break
+                    # clip打分
             
-            result = manual_bin_count(all_scores)
-            print(f"score分布", result)
-            print(f"未检测到数： {cnt_none_all}")
-            print(f"检测错误数： {cnt_false_all}")
-    
-        elif predict_mode == "clip":
-            with open(class_name+"_clip.pkl", "rb") as f:
-                values = pickle.load(f)
+            # 存
+            if predict_mode == "maskrcnn":
+                with open(save_dir+ class_name+".pkl", "wb") as f:
+                    pickle.dump(pred_instances, f)
+            elif predict_mode == "clip":
+                with open(save_dir+ class_name+"_clip.pkl", "wb") as f:
+                    pickle.dump(values, f)
+                
+                max_, min_ = 0,  1e4
+                dis = distances
+                for obj, obj_dict in values.items():
+                    for d in dis:
+                        value = values[obj][d]
+                        max_ = max(value) if max(value) > max_ else max_
+                        min_ = min(value) if min(value) < min_ else min_
+                print(f"clip score: max  {max_}, min {min_}")
+                
+        if mode == "read":
+            if predict_mode == "maskrcnn":
+                with open(class_name+".pkl", "rb") as f:
+                    pred_instances = pickle.load(f)
             
-            max_, min_ = 0,  1e4
-            dis = ['2m', '2.5m', '3m', '3.5m', '4m', '4.5m', '5m', '5.5m']
-            for obj, obj_dict in values.items():
-                for d in dis:
-                    value = values[obj][d]
-                    max_ = max(value) if max(value) > max_ else max_
-                    min_ = min(value) if min(value) < min_ else min_
-            print(f"clip score: max  {max_}, min {min_}")
-                # draw_array(obj, values[obj], class_name+"_clip10_norm", norm=True)
-        # 所有角度结果拼接
+                for obj, obj_dict in pred_instances.items():
+                    values[obj] = {}
+                    preds[obj] = {}
+                    cnt_none[obj] = {}
+                    cnt_false[obj] = {}
+                    for distance, instances_lst in obj_dict.items():
+                        values[obj][distance] = []
+                        preds[obj][distance] = []
+                        cnt_none[obj][distance] = 0
+                        cnt_false[obj][distance] = 0
+                        
+                        for angle, instance in enumerate(instances_lst):
+                            
+                            if len(instance) == 0:
+                                values[obj][distance].append(1)
+                                preds[obj][distance].append(None)
+                                cnt_none[obj][distance] += 1
+                            elif len(instance) == 1:
+                                if int(instance.pred_classes[0].cpu().numpy()) == 72: 
+                                    values[obj][distance].append(1- float(instance.scores.cpu().numpy()))
+                                    preds[obj][distance].append(float(instance.scores.cpu().numpy()))
+                                else:  # 错检
+                                    values[obj][distance].append(1)
+                                    cnt_false[obj][distance] += 1
+                            else:
+                                values[obj][distance].append(1 - float(instance.scores.mean().cpu().numpy()))
+                                preds[obj][distance].append(float(instance.scores.mean().cpu().numpy()))
+                        
+                # 统计
+                all_scores = []
+                cnt_none_all = 0
+                cnt_false_all = 0
+                for obj, obj_dict in pred_instances.items():
+                    draw_array(obj, values[obj], class_name)
+                    for dis, preds_scores in obj_dict.items():
+                        all_scores.extend(preds[obj][dis])
+                        cnt_none_all += cnt_none[obj][dis]
+                        cnt_false_all += cnt_false[obj][dis]
+                
+                result = manual_bin_count(all_scores)
+                print(f"score分布", result)
+                print(f"未检测到数： {cnt_none_all}")
+                print(f"检测错误数： {cnt_false_all}")
+        
+            elif predict_mode == "clip":
+                with open(class_name+"_clip.pkl", "rb") as f:
+                    values = pickle.load(f)
+                
+                max_, min_ = 0,  1e4
+                dis = distances
+                for obj, obj_dict in values.items():
+                    for d in dis:
+                        value = values[obj][d]
+                        max_ = max(value) if max(value) > max_ else max_
+                        min_ = min(value) if min(value) < min_ else min_
+                print(f"clip score: max  {max_}, min {min_}")
+                    # draw_array(obj, values[obj], class_name+"_clip10_norm", norm=True)
+            # 所有角度结果拼接
     
     
     
