081ab8c45c0aa9d712aa7f87fa2ddc3cb3d3caa2
yuanxuanS <panpanono@163.com>
2025-11-21 19:09:06 +0800

    track habitat for add pointnav

---
+++ third_parties/habitat/habitat/tasks/nav/camera_action.py
@@ -0,0 +1,298 @@
+from habitat import config
+import time
+import numpy as np
+import torch
+import copy
+
+from habitat.core.registry import registry
+from habitat.core.embodied_task import SimulatorTaskAction
+
+from habitat.sims.habitat_simulator.actions import HabitatSimActions
+from habitat_sim.utils.common import *
+from habitat.utils.geometry_utils import *
+from habitat.core.embodied_task import (
+    EmbodiedTask,
+    Measure,
+    SimulatorTaskAction,
+)
+import magnum as mn
+
+
+
+
+
+class BaseAction(SimulatorTaskAction):
+
+    def _get_uuid(self, *args, **kwargs) -> str:
+        raise NotImplementedError
+
+    def step(self, **kwargs):
+        """reference from habitat-sim/habitat_sim/simulator.py L251
+        """
+        self._sim._num_total_frames += 1
+
+        self._sim._last_state = self._sim.get_agent(0).get_state()
+
+        # step physics by dt=1.0 / 60.0
+        step_start_Time = time.time()
+        # self._sim._sim.step_physics(1.0 / 60.0)  # 这个是关于对物体施加作用力，应该与我们无关
+        _previous_step_time = time.time() - step_start_Time
+
+        sim_obs = self._sim.get_sensor_observations()
+        # sim_obs["collided"] = collided
+
+        self._sim._prev_sim_obs = sim_obs
+        observations = self._sim._sensor_suite.get_observations(sim_obs)
+
+        return observations
+
+    def _get_body_camera_angle(self):
+        """
+        获取当前body和camera角度（0~360°）
+        """
+        agent_state = self._sim.get_agent(0).get_state()
+        body_rotation = agent_state.rotation
+        body_angle = compute_heading_from_quaternion(agent_state.rotation)
+
+        sensor_name = list(agent_state.sensor_states.keys())[0]  # 第0个sensor的名字
+        sensor_rotation = agent_state.sensor_states[sensor_name].rotation
+        sensor_angle = compute_heading_from_quaternion(sensor_rotation)
+
+        sensor_rotation_acoord = self._sim.agents[0]._sensors['rgb'].node.rotation
+        return np.rad2deg(body_angle), np.rad2deg(sensor_angle), body_rotation, sensor_rotation, sensor_rotation_acoord
+
+    def _get_body_camera_pitch(self):
+        """
+        获取当前body和camera角度（0~360°）
+        sensor_rotation_acoord: sensor相对body的旋转
+        """
+        agent_state = self._sim.get_agent(0).get_state()
+        body_rotation = agent_state.rotation
+        body_pitch = compute_pitch_from_quaternion(agent_state.rotation)
+
+        sensor_name = list(agent_state.sensor_states.keys())[0]  # 第0个sensor的名字
+        sensor_rotation = agent_state.sensor_states[sensor_name].rotation
+        sensor_pitch = compute_pitch_from_quaternion(sensor_rotation)
+
+        sensor_rotation_acoord = self._sim.agents[0]._sensors['rgb'].node.rotation
+        return np.rad2deg(body_pitch), np.rad2deg(sensor_pitch), body_rotation, sensor_rotation, sensor_rotation_acoord
+    
+    def _cal_camera_angle(self, target_turn_angle, limited_angle=90):
+        '''
+        target_turn_angle： 绕y轴旋转角度(逆时针)
+        限制camera与body的角度不能超过一定度数
+        limited_angle: 机器人的camera和body夹角不大于limited_angle (可设置0~180)
+        
+        '''
+        # 获取当前body和camera角度
+        body_angle_new, camera_angle, body_rotation_new, camera_rotation, cam_rot_acoord = self._get_body_camera_angle()
+        camera_angle_tmp = -target_turn_angle + camera_angle        # heading方向是顺时针为正
+        if camera_angle_tmp- body_angle_new> limited_angle:
+            target_turn_angle = limited_angle + body_angle_new
+        elif body_angle_new- camera_angle_tmp > limited_angle:
+            target_turn_angle = -limited_angle + body_angle_new
+        else:
+            target_turn_angle = target_turn_angle
+        camera_rotation_new = self._move_camera_horizental(cam_rot_acoord, target_turn_angle)
+        # camera_angle_new = np.rad2deg(compute_heading_from_quaternion(camera_rotation_new))
+
+        # print("action_name:", self._get_uuid())
+        # print("body_angle_new:{} camera_angle_new:{} relative_angle:{} ".format(body_angle_new, camera_angle_new, (camera_angle_new - body_angle_new) % 360))
+        
+        
+        return camera_rotation_new
+
+    def quat_to_mnquat(self, quat):
+        mnquat = mn.Quaternion(mn.Vector3(quat.x, 
+                                        quat.y, 
+                                        quat.z), quat.w)
+        return mnquat
+    
+    def mnquat_to_quat(self, mnquat):
+        w = mnquat.scalar
+        x = mnquat.vector.x
+        y = mnquat.vector.y
+        z = mnquat.vector.z
+        quat = np.quaternion(w, x, y, z)
+        return quat
+        
+    def _move_camera_vertical(self, sensor_rotation, amount: float):
+        if not isinstance(sensor_rotation, mn.Quaternion):
+            sensor_rotation = self.quat_to_mnquat(sensor_rotation)
+        
+        # 对sensor_rotation根据上层(agent)坐标系的x轴旋转amount角度, 所以左乘
+        sensor_rotation_new =  mn.Quaternion.rotation(
+            mn.Deg(amount), mn.Vector3.x_axis()
+        ) * sensor_rotation
+        
+        # to quaternion type
+        sensor_rotation_new = self.mnquat_to_quat(sensor_rotation_new)
+        return sensor_rotation_new
+    
+    def _move_camera_horizental(self, sensor_rotation, amount: float):
+        '''
+        对sensor_rotation根据上层(agent)坐标系的-y轴旋转amount角度
+        '''
+        if not isinstance(sensor_rotation, mn.Quaternion):
+            sensor_rotation = self.quat_to_mnquat(sensor_rotation)
+            
+        sensor_rotation_new = sensor_rotation * mn.Quaternion.rotation(
+            mn.Deg(amount), - mn.Vector3.y_axis()       # 定义绕顺时针为正，所以y轴逆向
+        )
+        
+        # to quaternion type
+        sensor_rotation_new = self.mnquat_to_quat(sensor_rotation_new)
+        return sensor_rotation_new
+    
+    def _cal_camera_pitch(self, target_turn_angle, limited_pitch=60):
+        '''
+        对sensor相对agent的坐标cam_rot_acoord, 在agent坐标系中进行变换
+        limited_angle: 限制camera俯仰角度不能超过一定度数
+        
+        '''
+        body_pitch_new, camera_pitch, body_rotation_new, camera_rotation, cam_rot_acoord = self._get_body_camera_pitch()
+        # limit range
+        camera_pitch_tmp = target_turn_angle + camera_pitch
+        if camera_pitch_tmp > limited_pitch:
+            target_turn_angle = limited_pitch - camera_pitch
+        elif camera_pitch_tmp < -limited_pitch:
+            target_turn_angle = -limited_pitch - camera_pitch
+        else:
+            target_turn_angle = target_turn_angle
+        # 
+        camera_rot_acoord_new = self._move_camera_vertical(cam_rot_acoord, target_turn_angle)
+        # camera_pitch_new = np.rad2deg(compute_pitch_from_quaternion(camera_rot_acoord_new))
+
+        # print("action_name:", self._get_uuid())
+        # print("body_angle_new:{} camera_angle_new:{} relative_angle:{} ".format(body_angle_new, camera_angle_new, (camera_angle_new - body_angle_new) % 360))
+        return camera_rot_acoord_new
+    
+    def _implement_camera_action(self, camera_rotation_new):
+        '''把camera设置到某个角度
+        camera_angle_new: 需要设置到的角度（单位°）
+        '''
+        sensor_names = list(self._sim.agents[0]._sensors.keys())
+        for sensor_name in sensor_names:
+            sensor = self._sim.agents[0]._sensors[sensor_name].node
+            sensor.rotation = self.quat_to_mnquat(camera_rotation_new)
+        # self._sim.get_agent(0).set_state(agent_state)
+        # self._sim._sim.get_agent(0).set_state(agent_state, infer_sensor_states=False)
+    
+    
+@registry.register_task_action
+class CameraCaptureAction(BaseAction):
+    '''
+    相机捕获观测，认为episode结束
+    '''
+    def _get_uuid(self, *args, **kwargs) -> str:
+        return "camera_capture"
+
+    def step(self, *args, **kwargs):
+        kwargs['task'].is_found_called = False
+        # collided = self._sim._sim.get_agent(0).act(HabitatSimActions.MOVE_FORWARD)
+        a = self._cal_camera_angle(0)
+        observations = super().step()
+
+        return observations
+    
+
+
+
+@registry.register_task_action
+class CameraLeftAction(BaseAction):
+
+    def _get_uuid(self, *args, **kwargs) -> str:
+        return "camera_left"
+
+    def step(self, *args, **kwargs):
+        kwargs['task'].is_found_called = False
+
+        camera_delta = kwargs['task']._sim.habitat_config.CAMERA_TURN_ANGLE
+        relative_angle_limit = kwargs['task']._sim.habitat_config.RELATIVE_ANGLE_LIMIT
+        camera_rotation_new = self._cal_camera_angle(-camera_delta, relative_angle_limit)
+        self._implement_camera_action(camera_rotation_new)
+
+        observations = super().step()
+
+        return observations
+
+
+@registry.register_task_action
+class CameraRightAction(BaseAction):
+
+    def _get_uuid(self, *args, **kwargs) -> str:
+        return "camera_right"
+
+    def step(self, *args, **kwargs):
+        kwargs['task'].is_found_called = False
+
+        camera_delta = kwargs['task']._sim.habitat_config.CAMERA_TURN_ANGLE
+        relative_angle_limit = kwargs['task']._sim.habitat_config.RELATIVE_ANGLE_LIMIT
+        camera_rotation_new = self._cal_camera_angle(camera_delta, relative_angle_limit)
+        self._implement_camera_action(camera_rotation_new)
+
+        observations = super().step()
+
+        return observations
+
+@registry.register_task_action
+class CameraUpAction(BaseAction):
+
+    def _get_uuid(self, *args, **kwargs) -> str:
+        return "camera_up"
+
+    def step(self, *args, **kwargs):
+        kwargs['task'].is_found_called = False
+
+        camera_delta = kwargs['task']._sim.habitat_config.CAMERA_TURN_ANGLE
+        relative_angle_limit = kwargs['task']._sim.habitat_config.CAMERA_PITCH_LIMIT
+        camera_rot_acoord_new = self._cal_camera_pitch(camera_delta, relative_angle_limit)
+        self._implement_camera_action(camera_rot_acoord_new)
+
+        observations = super().step()
+
+        return observations
+
+@registry.register_task_action
+class CameraDownAction(BaseAction):
+
+    def _get_uuid(self, *args, **kwargs) -> str:
+        return "camera_down"
+
+    def step(self, *args, **kwargs):
+        kwargs['task'].is_found_called = False
+
+        camera_delta = kwargs['task']._sim.habitat_config.CAMERA_TURN_ANGLE
+        relative_angle_limit = kwargs['task']._sim.habitat_config.CAMERA_PITCH_LIMIT
+        camera_rotation_new = self._cal_camera_pitch(-camera_delta, relative_angle_limit)
+        self._implement_camera_action(camera_rotation_new)
+
+        observations = super().step()
+
+        return observations
+    
+# def joint_action(body_action, camera_action, cfg_AC: config, time_steps, test_new_baseline = None):
+#     fourAction2tenAction = {0:0, 1:1, 2:5, 3:9}
+
+#     if cfg_AC.camera_AC == 'none':   # multiON的设置，输出4个动作
+#         b_action = torch.zeros_like(body_action)
+#         for i in range(len(body_action)):
+#             b_action[i] = fourAction2tenAction[body_action[i].item()]
+#         action = b_action
+#     elif cfg_AC.body_AC == 'e2e' and cfg_AC.camera_AC == 'e2e': # e2e同时决定body和camera，输出10个动作
+#         action = body_action
+#     else:
+#         mask = (body_action-1)>=0   # 判断是否执行found
+
+#         if test_new_baseline is not None:
+#             for idx, flag in enumerate(test_new_baseline):
+#                 if flag:
+#                     body_action[idx] = camera_action[idx] + 1 # only for another baseline testing
+
+#         action = (body_action-1)*3 + camera_action + 1
+#         action *= mask
+
+#     action[time_steps < 3] = 5   # 前三步左转左看
+#     body_action[time_steps < 3] = 2
+#     camera_action[time_steps < 3] = 1
+#     return action
---
+++ third_parties/habitat/habitat/utils/geometry_utils.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import List, Tuple, Union
+
+import numpy as np
+import quaternion
+import math
+EPSILON = 1e-8
+
+
+def angle_between_quaternions(q1: np.quaternion, q2: np.quaternion) -> float:
+    r"""Returns the angle (in radians) between two quaternions. This angle will
+    always be positive.
+    从 {q}_1 旋转到 {q}_2 所需的最小旋转角
+    """
+    q1_inv = np.conjugate(q1)
+    dq = quaternion.as_float_array(q1_inv * q2)
+
+    return 2 * np.arctan2(np.linalg.norm(dq[1:]), np.abs(dq[0]))
+
+
+def quaternion_from_two_vectors(v0: np.array, v1: np.array) -> np.quaternion:
+    r"""Computes the quaternion representation of v1 using v0 as the origin."""
+    v0 = v0 / np.linalg.norm(v0)
+    v1 = v1 / np.linalg.norm(v1)
+    c = v0.dot(v1)
+    # Epsilon prevents issues at poles.
+    if c < (-1 + EPSILON):
+        c = max(c, -1)
+        m = np.stack([v0, v1], 0)
+        _, _, vh = np.linalg.svd(m, full_matrices=True)
+        axis = vh.T[:, 2]
+        w2 = (1 + c) * 0.5
+        w = np.sqrt(w2)
+        axis = axis * np.sqrt(1 - w2)
+        return np.quaternion(w, *axis)
+
+    axis = np.cross(v0, v1)
+    s = np.sqrt((1 + c) * 2)
+    return np.quaternion(s * 0.5, *(axis / s))
+
+
+def quaternion_to_list(q: np.quaternion):
+    return q.imag.tolist() + [q.real]
+
+
+def quaternion_from_coeff(coeffs: np.ndarray) -> np.quaternion:
+    r"""Creates a quaternions from coeffs in [x, y, z, w] format"""
+    quat = np.quaternion(0, 0, 0, 0)
+    quat.real = coeffs[3]
+    quat.imag = coeffs[0:3]
+    return quat
+
+
+def quaternion_rotate_vector(quat: np.quaternion, v: np.array) -> np.array:
+    r"""Rotates a vector by a quaternion; 
+        将向量v执行quat旋转
+    Args:
+        quaternion: The quaternion to rotate by
+        v: The vector to rotate
+    Returns:
+        np.array: The rotated vector
+    """
+    vq = np.quaternion(0, 0, 0, 0)
+    vq.imag = v
+    return (quat * vq * quat.inverse()).imag
+
+
+def agent_state_target2ref(
+    ref_agent_state: Union[List, Tuple], target_agent_state: Union[List, Tuple]
+) -> Tuple[np.quaternion, np.array]:
+    r"""Computes the target agent_state's rotation and position representation
+    with respect to the coordinate system defined by reference agent's rotation and position.
+    All rotations must be in [x, y, z, w] format.
+
+    :param ref_agent_state: reference agent_state in the format of [rotation, position].
+         The rotation and position are from a common/global coordinate systems.
+         They define a local coordinate system.
+    :param target_agent_state: target agent_state in the format of [rotation, position].
+        The rotation and position are from a common/global coordinate systems.
+        and need to be transformed to the local coordinate system defined by ref_agent_state.
+    """
+
+    assert (
+        len(ref_agent_state[1]) == 3
+    ), "Only support Cartesian format currently."
+    assert (
+        len(target_agent_state[1]) == 3
+    ), "Only support Cartesian format currently."
+
+    ref_rotation, ref_position = ref_agent_state
+    target_rotation, target_position = target_agent_state
+
+    # convert to all rotation representations to np.quaternion
+    if not isinstance(ref_rotation, np.quaternion):
+        ref_rotation = quaternion_from_coeff(ref_rotation)
+    ref_rotation = ref_rotation.normalized()
+
+    if not isinstance(target_rotation, np.quaternion):
+        target_rotation = quaternion_from_coeff(target_rotation)
+    target_rotation = target_rotation.normalized()
+
+    rotation_in_ref_coordinate = ref_rotation.inverse() * target_rotation
+
+    position_in_ref_coordinate = quaternion_rotate_vector(
+        ref_rotation.inverse(), target_position - ref_position
+    )
+
+    return (rotation_in_ref_coordinate, position_in_ref_coordinate)
+
+def compute_heading_from_quaternion(r):
+    """
+    计算r的朝向相对世界坐标系的正朝向的角度, 顺时针为正
+    r的Y上，Z为负朝向，X为向右
+    r - rotation quaternion
+    
+    Computes clockwise rotation about Y.
+    """
+    # quaternion - np.quaternion unit quaternion
+    # Real world rotation
+    direction_vector = np.array([0, 0, -1])  # Forward vector
+    # 将向量 direction_vector 从 Agent 坐标系转换到世界坐标系
+    heading_vector = quaternion_rotate_vector(r, direction_vector)     
+    # 向量第二个值取负，是在朝向上的投影；
+    # arctan2(y,x)函数默认从x逆时针转向y，所以取负反方向；   
+    phi = -np.arctan2(heading_vector[0], -heading_vector[2]).item()     # [-pi, pi]
+    return phi
+
+
+
+def compute_quaternion_from_heading(theta):
+    """
+    Setup: -Z axis is forward, X axis is rightward, Y axis is upward.
+    theta - heading angle in radians --- measured clockwise from -Z to X.
+
+    Compute quaternion that represents the corresponding clockwise rotation about Y axis.
+    """
+    # Real part
+    q0 = math.cos(-theta / 2)
+    # Imaginary part
+    q = (0, math.sin(-theta / 2), 0)
+
+    return np.quaternion(q0, *q)
+
+def compute_pitch_from_quaternion(r: np.quaternion) -> float:
+    """
+    计算 Agent 坐标系r 相对于世界 XZ 平面（水平面）的俯仰角 (Pitch)。
+    
+    使用 numpy-quaternion 库。
+
+    基于 Agent 坐标系定义：
+    Y 轴 = 向上 (Up)
+    X 轴 = 向右 (Right)
+    Z 轴 = 向后 (Backward)
+
+    Args:
+        r (np.quaternion): Agent 在世界坐标系中的朝向四元数。
+
+    Returns:
+        float: 俯仰角 (Pitch)，单位为弧度。正值表示向上看，负值表示向下看。
+    """
+    
+    ## agent朝向向量
+    v_local_forward = np.array([0.0, 0.0, -1.0])
+    # 将向量 v_local_forward 从 Agent 坐标系转换到世界坐标系
+    v_world_forward = quaternion.rotate_vectors(r, v_local_forward)
+    # 获取向量 v_world_forward 的 y 分量
+    argument = v_world_forward[1]
+    # 使用 np.clip 确保参数在 [-1.0, 1.0] 范围内，以应对浮点误差 
+    argument = np.clip(argument, -1.0, 1.0)
+    
+    pitch = np.arcsin(argument)
+    
+    return pitch
+
+def create_pitch_quaternion_local(pitch_angle_rad: float) -> np.quaternion:
+    """
+    构造绕物体局部 X 轴旋转 pitch_angle_rad 的四元数。
+    
+    Args:
+        pitch_angle_rad: 要增加的俯仰角，单位为弧度。
+
+    Returns:
+        np.quaternion: 表示该俯仰旋转的四元数。
+    """
+    half_angle = pitch_angle_rad / 2
+    
+    # q = cos(phi/2) + sin(phi/2) * i
+    # 局部 X 轴为旋转轴 (1, 0, 0)
+    q_pitch = np.quaternion(
+        math.cos(half_angle),  # 实部 (w)
+        math.sin(half_angle),  # 虚部 i (对应 X 轴)
+        0.0,                   # 虚部 j (对应 Y 轴)
+        0.0                    # 虚部 k (对应 Z 轴)
+    )
+    return q_pitch
+
+def apply_local_rotation(rotation_world: np.quaternion, rotation_local_delta: np.quaternion) -> np.quaternion:
+    """
+    计算物体在世界坐标系下的新旋转位姿。
+    
+    Args:
+        rotation_world: 物体当前在世界坐标系中的旋转位姿 (r)。
+        rotation_local_delta: 要在物体坐标系下施加的额外旋转 (dr)。
+        
+    Returns:
+        np.quaternion: 物体在世界坐标系下的新旋转位姿 (r_new)。
+    """
+    
+    # 局部旋转（在物体自身坐标系下施加旋转）
+    # 公式: r_new = r * dr
+    rotation_new = rotation_world * rotation_local_delta
+    
+    return rotation_new
+
