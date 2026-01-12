b2472ff6e0311e6d046d12a5ffdc81cddcf9fdd7
yuanxuanS <panpanono@163.com>
2026-01-12 14:25:15 +0800

    tmp

--- test_re_log.py
+++ test_re_log.py
@@ -3,9 +3,9 @@ import re
 import matplotlib.pyplot as plt
 
 # 日志文件路径（替换为实际路径）
-note = "vsqf_v3_2"
-reward =    "dis reward"       # "vsqf reward" #    #  "episode mean reward" #
-log_file_path = "/home/users/wpp/Semantic-Curiosity/Semantic-Curiosity/rl_vsqf_v4_1.log"
+note = "vsqf_v1_4_rec2-2"
+reward =    "score abs r"       # "vsqf reward" #    #  "episode mean reward" #
+log_file_path = "/home/wpp/Seman-Curiosity/rl_vsqf_v1_4_re_conti2.log"
 # "/home/users/wpp/Semantic-Curiosity/Semantic-Curiosity/rl_vsqf_v3_1.log"
 # "/home/users/wpp/Semantic-Curiosity/Semantic-Curiosity/logs/expv7.log"
 # "/home/users/wpp/Semantic-Curiosity/Semantic-Curiosity/rl_vsqf_v3_0.log"
