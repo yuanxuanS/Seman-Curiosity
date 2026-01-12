b8f74e0a4d8a8f57c61d57202a54e9a2b179e554
yuanxuanS <panpanono@163.com>
2026-01-12 14:24:57 +0800

    tmp

--- test_re_log.py
+++ test_re_log.py
@@ -3,8 +3,9 @@ import re
 import matplotlib.pyplot as plt
 
 # 日志文件路径（替换为实际路径）
-note = "rl_curiosity"
-reward ="episode mean reward" #"episode mean reward" #"dis reward"       # "vsqf reward" #    # 
+note = "rl_tp_diver2_m3_rs2"
+reward ="episode mean diver reward" #"dis reward"  
+cumu = False 
 log_file_path = f"/home/users/wpp/Semantic-Curiosity/Semantic-Curiosity/{note}.log"
 # 读取文件内容
 with open(log_file_path, 'r') as file:
@@ -25,6 +26,9 @@ if not rewards:
     print(f"未找到 {reward} 数据！")
 else:
     print(len(rewards))
+    if cumu:
+        for i in range(len(rewards) - 1, 0, -1):
+            rewards[i] = rewards[i] - rewards[i-1]
     # 绘制曲线
     plt.plot(rewards, label=f'{reward}')
     plt.xlabel('Episode')
@@ -34,3 +38,5 @@ else:
     plt.grid(True)
     # plt.show()
     plt.savefig(f'{note}_{reward}.png')  # 保存图像
+    
+    
